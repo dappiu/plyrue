f464e8288556b82a0c6b450806a3e9c49e5baafe
Davide Rossi <dappiu@gmail.com>
2019-04-07 17:53:36 +0200

    Makes the plugin accepting default args. Exports only the base component with the added install method

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
-import PlyrUe from "./components/PlyrUe.vue"
-import 'plyr/dist/plyr.css'
+import PlyrUe from './PlyrUe.vue'
 
-const PlyrUePlugin = {
-  install(vue) {
-    vue.component(PlyrUe.name, PlyrUe);
+PlyrUe.install = (Vue, options = {}) => {
+  if (options.plyr) {
+    PlyrUe.props.options.default = () => { return { ...options.plyr } }
   }
+  if (options.emit) {
+    PlyrUe.props.emit.default = () => { return [ ...options.emit ] }
+  }
+  Vue.component(PlyrUe.name, PlyrUe)
 }
 
-
+// Credit to https://github.com/irazasyed for this auto Vue.use() when
+// installing from unpkg or similar.
 if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(PlyrUePlugin)
+  window.Vue.use(PlyrUe)
 }
 
-export {
-  PlyrUe,
-  PlyrUePlugin
-}
-
-export default {
-  PlyrUePlugin
-}
+export default PlyrUe
